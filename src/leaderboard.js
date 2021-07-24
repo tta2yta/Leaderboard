8810add3d549444857eb408f90d78a51b8d6b2bc
tta2yta <tta2yta@gmail.com>
2021-07-25 00:19:16 +0300

    Add createGame method

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -4,6 +4,15 @@ class LeaderBoard{
     }
 
      createGame=()=>{
+         const Http=new XMLHttpRequest();
+         const url="";
+         Http.open("POST", url);
+         Http.send();
+
+         Http.onreadystatechange=function(){
+             if(this.readyState===4 && this.status==400)
+             console.log(Http.responseText)  
+         }
 
     }
 }
