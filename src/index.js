d3189e124507b8c094b900d4c670abb293194ab1
tta2yta <tta2yta@gmail.com>
2021-07-25 00:28:42 +0300

    Modify createGame method

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './style.css'
+import LeaderBoard from './leaderboard'
+
+const game=new LeaderBoard();
+game.createGame();
--- src/leaderboard.js
+++ src/leaderboard.js
@@ -1,13 +1,17 @@
-class LeaderBoard{
+export default class LeaderBoard{
     constructor(){
 
     }
 
-     createGame=()=>{
+     createGame=(name)=>{
+         const gameName={name:name}
          const Http=new XMLHttpRequest();
-         const url="";
+         const url="https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
+         Http.setRequestHeader('Content-Type', 'application/json');
          Http.open("POST", url);
-         Http.send();
+         Http.send(JSON.stringify({
+            value: {gameName}
+        }));
 
          Http.onreadystatechange=function(){
              if(this.readyState===4 && this.status==400)
